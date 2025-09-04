5fe35c46054eacdff4f96298ad8cc27daa5e407b
MD. MEHEDI HASAN <mehedihasan811914@gmail.com>
2025-09-04 22:57:21 +0600

    Create ArithmaticOperation.java

---
+++ ArithmaticOperation.java
@@ -0,0 +1,22 @@
+/* addition, subtraction, division, multiplacation & modulas
+ operation in two number.
+ */
+public class ArithmaticOperation {
+    public static void main(String[] args){
+        int a= 30;
+        int b= 20;
+
+
+        int additon = a+b;
+        int subtraction = a-b;
+        double division = a/b;
+        int multiplacation = a*b;
+        double mudulas = a%b;
+
+        System.out.println("addition: " + additon);
+        System.out.println("subtraction: " + subtraction);
+        System.out.println("division: "+ division);
+        System.out.println("multiplacation: "+ multiplacation);
+        System.out.println("mudulas: "+ mudulas);
+    }
+}
