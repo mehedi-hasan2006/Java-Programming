3bc6e7198e1c305b08fc1e3e1ae759cb6fbfe789
MD. MEHEDI HASAN <mehedihasan811914@gmail.com>
2025-09-28 23:18:44 +0600

    Create Day_8.java

---
+++ Day_8.java
@@ -0,0 +1,15 @@
+public class Day_8 {
+    public static void main(String[] args){
+        // loop = for loop, while loop, do-while
+
+
+       /* for(counter_varibale; condition; increment/decrement){
+            Statement
+        } */
+
+
+        for ( int i=1; i<5; i++){  // i++ = i+1
+            System.out.println("Sorry");
+        }
+    }
+}
