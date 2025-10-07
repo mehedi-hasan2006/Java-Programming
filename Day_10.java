9c56d48a0a87a1cd271c5af62ba0df237c396c0f
MD. MEHEDI HASAN <mehedihasan811914@gmail.com>
2025-10-07 22:32:06 +0600

    Create Day_10.java

---
+++ Day_10.java
@@ -0,0 +1,34 @@
+public class Day_10 {
+    public static void main(String[] args) {
+        // While loop syntax
+
+//        int counter =0;   (while loop syntax)
+//        while(condition){
+//            statement
+//                    increment / decrement
+//        }
+
+        // syntax for loop
+//        for(count; condition; updation){
+//            statement
+//        }
+//
+
+      //  syntax while loop //
+//        count;
+//        while (conditon){
+//            statement
+//                    increment/ decrement / updation
+//        }
+
+        int counter=1;
+        while (counter<31){
+            System.out.println(counter);
+            counter+=2;
+        }
+
+        for (int count=0; count<5; count++){
+            System.out.println("While Loop");
+        }
+    }
+}
