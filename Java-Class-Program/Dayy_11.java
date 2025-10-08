3964d64dcc7e255d9f0c4c42a049c88da2b2892a
MD. MEHEDI HASAN <mehedihasan811914@gmail.com>
2025-10-09 00:08:10 +0600

    Add files via upload

---
+++ Java-Class-Program/Dayy_11.java
@@ -0,0 +1,24 @@
+public class Dayy_11 {
+    public static void main(String[] args) {
+
+        int sum=15;                      // 0+1=1    15, 14 13 12
+        int counter=6;
+        while (counter<=5){
+            sum=sum+counter;
+            System.out.println(sum);
+            counter++;
+        }
+
+        int summ=0;
+        for (int i=1; i<=5; i++){
+            summ=summ+i;
+            System.out.println(summ);
+        }
+
+        int a=10;
+        while (a>=1){
+            System.out.println("sorry");
+            a--;
+        }
+    }
+}
