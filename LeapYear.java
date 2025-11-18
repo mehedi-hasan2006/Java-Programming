2a7f2f2b2c0a345a34c2b8f8bd6f7f0697d8a68b
MD. MEHEDI HASAN <mehedihasan811914@gmail.com>
2025-11-18 08:35:05 +0600

    Enhance LeapYear program with printf method
    Added a new implementation of the Leap Year program using printf for output formatting.

--- LeapYear.java
+++ LeapYear.java
@@ -16,3 +16,22 @@ public class LeapYear {
         }
     }
 }
+
+// Find Leap Year using printf method
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args) {
+
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.print("Enter Year : ");
+        int year= scanner.nextInt();
+
+        if((year%4==0 ) && (year %100!=0) || (year%400==0)){
+            System.out.printf(" %d is a leap year",  year);
+        }
+        else{
+            System.out.printf("%d is not a leap year", year);
+        }
+    }
+}
